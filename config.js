a10a310bcb7660fb543891a73ff1f1ed9f83738f
Rizky oktavian <141605878+Rizkygamers@users.noreply.github.com>
2026-02-18 15:58:33 +0700

    update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 global.mess = {
-  wait: '⏳ Sedang diproses...',
-  error: '🚫 Terjadi kesalahan, coba lagi nanti.',
-  admin: 'Fitur ini hanya untuk admin grup!',
-  default: 'Perintah Tidak dikenal?!',
-    group: 'Fitur ini hanya bisa digunakan di dalam grup!',
-  }
-  
-global.image = "https://raw.githubusercontent.com/Rizkygamers/waifuim-img/main/uploads/image-1769614337905.jpg"
-global.owner = ['62895417273523']
-global.idch = "120363402305551203@newsletter"
-global.namach = "BaseBot"
-  
-global.prefix = ['!', '.'];
-  
+    wait: "⏳ Sedang diproses...",
+    error: "🚫 Terjadi kesalahan, coba lagi nanti.",
+    admin: "Fitur ini hanya untuk admin grup!",
+    owner: "Fitur ini hanya untuk owner!",
+    default: "Perintah Tidak dikenal?!",
+    group: "Fitur ini hanya bisa digunakan di dalam grup!"
+};
+
+global.image =
+    "https://raw.githubusercontent.com/Rizkygamers/waifuim-img/main/uploads/image-1769614337905.jpg";
+global.owner = ["62895417273523"];
+global.idch = "120363402305551203@newsletter";
+global.namach = "BaseBot";
+global.prefix = ["!", "."];
